f995586e3bfb144f3e83d6eda25c97a740feb390
sumod badchhape <badchhapesumod100@gmail.com>
2020-06-29 12:20:27 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,63 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+function showLoadingSpinner() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function hideLoadingSpinner() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+    }
+}
+
+//get quote from API
+async function getQuote() {
+    showLoadingSpinner();
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const apiURL = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response = await fetch(proxy + apiURL);
+        const data = await response.json();
+        //if author is blank then add unknown
+        if (data.quoteAuthor === '') {
+            authorText.innerText = 'Unknown';
+        }
+        else {
+            authorText.innerText = data.quoteAuthor;
+        }
+        //reduce font size for quote larger than 120 character
+        if (data.quoteText.length > 120) {
+            quoteText.classList.add('long-quote');
+        }
+        else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = data.quoteText;
+        hideLoadingSpinner();
+    }
+    catch (error) {
+        getQuote();
+    }
+}
+
+//Tweet Quote
+function tweet() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterURL = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterURL, '_blank');
+}
+
+//Event listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweet);
+
+//on init
+getQuote();
